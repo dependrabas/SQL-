3c811f808e71dbb894d6366a813fec56cf265249
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-16 17:39:54 +0530

    Add files via upload

---
+++ Database1.sql
@@ -0,0 +1,8 @@
+show databases;
+Create database dependraDB;
+use dependraDB;
+Create table Tblemployee(emp_code int, emp_name varchar(200), age int);
+Insert into Tblemployee values(1, 'Dependra Basnet',20);
+Insert into Tblemployee values(2, 'Depen',21);
+Insert into Tblemployee values(3, 'Basnet',22);
+Describe Tblemployee;
