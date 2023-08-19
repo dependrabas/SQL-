96752ff478c6b6f86305bdc041a642b9d4c38cbb
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-08-19 10:12:32 +0530

    Add files via upload

---
+++ Student_Database.11sql.sql
@@ -0,0 +1,23 @@
+ show databases;
+create database Student;
+use Student;
+create table Student;
+INSERT INTO Student (Rno, Name, DOB, Gender, Class, College, City, Marks)
+VALUES
+    (1, 'Sonu', '2000-05-15', 'Male', '10th', 'LPU College', 'AAamritsar', 85.5);
+
+INSERT INTO Student (Rno, Name, DOB, Gender, Class, College, City, Marks)
+VALUES
+    (2, 'Kuenley', '2001-02-20', 'Female', '11th', 'RUB University', 'Los Angeles', 92.0);
+
+INSERT INTO Student (Rno, Name, DOB, Gender, Class, College, City, Marks)
+VALUES
+    (3, 'Sonam', '2002-09-10', 'Male', '9th', 'Dakin Institute', 'Chicago', 28.3);
+
+INSERT INTO Student (Rno, Name, DOB, Gender, Class, College, City, Marks)
+VALUES
+    (4, 'Phuentsho', '2003-07-03', 'Female', '12th', 'GNH College', 'Patiala', 95.7);
+
+INSERT INTO Student (Rno, Name, DOB, Gender, Class, College, City, Marks)
+VALUES
+    (5, 'Depen', '2001-09-14', 'Male', '12th', 'Chitkara University', 'Thimphu', 88.2);
